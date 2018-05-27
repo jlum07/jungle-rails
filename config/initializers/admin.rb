20626472e121c9653f3a0bf86f1ca5aea049ca44
Justin <j.lum07@gmail.com>
2018-05-26 23:09:56 -0400

    📧 email order

---
+++ config/initializers/admin.rb
@@ -0,0 +1,6 @@
+Rails.configuration.admin = {
+  :admin_username => ENV['ADMIN_USERNAME'],
+  :admin_password => ENV['ADMIN_PASSWORD']
+}
+
+# Stripe.api_key = Rails.configuration.stripe[:secret_key]
